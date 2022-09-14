e61a33d852786d54191085dbb325375e20fdb7b0
kelvinkauan <kelvinkauan9@gmail.com>
2022-09-14 17:30:31 -0300

    criação do banco de dados do Administrador, criação dos arquivos administrador no repository, models, controllers, e views.

---
+++ app/controllers/AdmController.php
@@ -0,0 +1,28 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    
+require_once __DIR__ . "/../repository/AdmRepository.php";
+
+    $administrador = new AdmController();
+
+    class AdmController{
+        
+
+
+        
+    }
+
+
+
+
+
+    ?>
+
+
+
+
+
+
+
