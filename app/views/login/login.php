e70349cd928f6fb19c6123c13cbe3a3278ea4674
mattheusMSL <matthewpeterparker@gmail.com>
2022-09-27 04:06:51 -0300

    html do login
    a

---
+++ app/views/login/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login bkmeventos </title>
+</head>
+<body>
+    <div class="login">
+     <label for="idlog"> Login: </label>
+      <input type="text" name="nome" id="idlog">
+       <p/>
+       <label for="idpass"> Senha: </label>
+      <input type="password" name="senha" id="idpass">
+    </div>
+</body>
+</html>
