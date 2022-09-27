7be0f8b9d2db690604d0e8266db7a7d60db24a7a
mattheusMSL <matthewpeterparker@gmail.com>
2022-09-27 05:14:01 -0300

    algumas atualizações e cadastro com senha criptofragada

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -4,17 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login bkmeventos </title>
+    <title>bkmeventos login</title>
 </head>
 <body>
+
+    <h2>Login Organizador</h2>
+
     <div class="login">
-     <label for="idlog"> Login: </label>
-      <input type="text" name="nome" id="idlog">
-       <p/>
-       <label for="idpass"> Senha: </label>
-      <input type="password" name="senha" id="idpass">
-     <br>
-    <a href="../organizadores/CadastroOrganizador.php">Cadastre-se aqui</a>
- </div>
+     <form action="./organizadorController.php?action=login" method="POST">
+      <label for="idlog"> Login: </label>
+       <input type="text" name="nome" id="idlog" required placeholder="Nome Completo...">
+        <p/>
+         <label for="idpass"> Senha: </label>
+           <input type="password" name="senha" id="idpass" required placeholder="Senha...">
+          <br>
+         <input type="submit" value="login">
+       <br>
+      <a href="../organizadores/CadastroOrganizador.php">Cadastre-se aqui</a>
+     </form>
+    </div>
+    
 </body>
 </html>
