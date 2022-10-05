4cc8bfaee34057902301eca6db5079c08be56720
kelvinkauan <kelvinkauan9@gmail.com>
2022-10-05 17:16:46 -0300

    mudando o index

--- app/index.php
+++ app/index.php
@@ -2,8 +2,9 @@
 
 /* Redireciona o usúario para o controller principal dele no caso o CRUD (create,read,update,delete) //colocar no header?action=findAll */
 
-//header("location: controllers/OrganizadorController.php?action=findAll");
+
+header("location: controllers/OrganizadorController.php?action=findAll");
 
 //header("location: controllers/AdmController.php?action=findALL");
-header("location: views/login/login.php");
+//header("location: views/login/login.php");
 //header("location: controllers/OrganizadorController.php?action=findAll");
