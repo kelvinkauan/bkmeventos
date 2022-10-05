623aa2a6d09b816465d7c0c2bc8226df4c7f95b3
kelvinkauan <kelvinkauan9@gmail.com>
2022-10-05 17:14:33 -0300

    mudando o html do login

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -1,19 +1,22 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login bkmeventos </title>
+    <title>bkmeventos login</title>
 </head>
 <body>
-    <form method="POST" action="/../controller/OrganizadorController.php"></form>
+    <h2>Login Organizador</h2>
     <div class="login">
-     <label for="idlog"> Login: </label>
-      <input type="text" name="nome" id="idlog">
-       <p/>
-       <label for="idpass"> Senha: </label>
-      <input type="password" name="senha" id="idpass">
+     <form action="./OrganizadorController.php?action=login" method="POST">
+      <label for="idlog"> Login: </label>
+       <input type="text" name="nome" id="idlog" placeholder="Nome completo...">
+        <p/>
+         <label for="idpass"> Senha: </label>
+         <input type="password" name="senha" id="idpass"  placeholder="Senha...">
+        <p/>
+       <input type="submit" value="login">
+     </form>
     </div>
 </body>
 </html>
