aed2bf7e18052871fd51075db8adca8c4a9d3857
kelvinkauan <kelvinkauan9@gmail.com>
2022-10-26 16:57:14 -0300

    xx

--- organizadores.sql
+++ organizadores.sql
@@ -1,15 +1,3 @@
 use projeto_integrador;
 
-#create table organizadores(
-#	idOrganizador int not null auto_increment,
- #   nc_Organizador varchar(60),
-  #  email_Organizador varchar (60),
-   # senha_Organizador varchar(12)
-#);
- #create table administradores(
- #idAdministrador int not null auto_increment,
- #nome_Adm varchar(10),
- #senha_Adm varchar(10),
- #);
- alter table administradores change senhaAdm senha_Adm varchar(10) ;
-desc administradores;
+desc cadastrar_evento;
