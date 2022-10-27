66addcf86af462a61a1cb730ab60907a30b1c38b
mattheusMSL <matthewpeterparker@gmail.com>
2022-10-27 01:09:47 -0300

    att

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -11,41 +11,38 @@
 
 <body>
 
-
 <div class="container" id="container">
  <div class="form-container sign-up-container">
   <form action="./OrganizadorController.php?action=create" method="POST">
    <h1>Crie sua conta</h1>
     <span>use seu e-mail para se registrar</span>
-	 <input type="text" name="nome" placeholder="Nome" required/>
-	  <input type="email" name="email" placeholder="Email" required/>
-	  <input type="password" name="senha" placeholder="Senha" required/>
-	  <button type="reset" value="Limpar">limpar</button>
-	 <br>
-    <button type="submit" value="Cadastrar">Cadastrar</button>
-   </form>
-  </div>
-
+	   <input type="text" name="nome" placeholder="Nome" required/>
+	    <input type="email" name="email" placeholder="Email" required/>
+	    <input type="password" name="senha" placeholder="Senha" required/>
+	   <button type="reset" value="Limpar">limpar</button>
+	  <br>
+   <button type="submit" value="Cadastrar">Cadastrar</button>
+  </form>
+ </div>
  <div class="form-container sign-in-container">
   <form action="./OrganizadorController.php?action=login" method="POST">
    <h1>Faça Login</h1>
-	<span> insira seus dados</span>
-	 <input type="email" name="email"placeholder="Email" />
-	  <input type="password" name="senha" placeholder="Senha" />
-	 <a href="#">Esqueceu sua senha?</a>
-	<button type="submit" value="login">Entrar</button>
+    <span> insira seus dados</span>
+	   <input type="email" name="email"placeholder="Email" />
+	    <input type="password" name="senha" placeholder="Senha" />
+     <a href="#">Esqueceu sua senha?</a>
+	  <button type="submit" value="login">Entrar</button>
    <input value="1" name="login" type="hidden">
   </form>
  </div>
- 
  <div class="overlay-container">
   <div class="overlay">
    <div class="overlay-panel overlay-left">
     <h1>Bem-vindo de volta!</h1>
      <p>Para se manter conectado, faça login com seus dados pessoais</p>
-	  <button class="ghost" id="signIn">Entrar</button>
-	   </div>
-	   <div class="overlay-panel overlay-right">
+	    <button class="ghost" id="signIn">Entrar</button>
+	     </div>
+	     <div class="overlay-panel overlay-right">
       <h1>Olá!</h1>
      <p>Insira seus dados pessoais para criar uma conta</p>
     <button class="ghost" id="signUp">Cadastre-se</button>
@@ -53,9 +50,6 @@
   </div>
  </div>
 </div>
-
-<script src="../views/styleLogin/login.js" charset="utf-8"></script>
-
-</body>
-
+ <script src="../views/styleLogin/login.js" charset="utf-8"></script>
+ </body>
 </html>
