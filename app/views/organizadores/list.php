a22a091e627317b595ba8a06c38c17fa39d85788
mattheusMSL <matthewpeterparker@gmail.com>
2022-11-15 12:05:29 -0300

    mini atts

--- app/views/organizadores/list.php
+++ app/views/organizadores/list.php
@@ -6,7 +6,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <meta http-equiv="Content-Type" content="text/html; charset=utf8">
     <link rel="stylesheet" type="text/css" href="../views/helpers/estilos.css">
     <script src="../views/helpers/funcoescrud.js" type="text/javascript"></script>
