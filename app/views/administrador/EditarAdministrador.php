8c0a27a91be839fa53a534716656c39d99fe3247
mattheusMSL <matthewpeterparker@gmail.com>
2022-11-17 09:50:08 -0300

    atualizações com css no editar adm

--- app/views/administrador/EditarAdministrador.php
+++ app/views/administrador/EditarAdministrador.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../views/administrador/cssAdm/editaradm.css">
     <title>editar administrador</title>
 </head>
 <body>
@@ -11,25 +12,36 @@
 
 include_once __DIR__ . "/../helpers/mensagem.php";
 
-
 ?>
 <div class="main-edit"> 
-    <div class="middle-main">  
-<h2> Editar administrador </h2>
-<p/>
+ <div class="middle-main">  
+  <h2> Editar administrador </h2>
+ <p/>
 </div>
+<div class="right-main">
+   <h3>Mantenha seus dados sempre atualizados</h3>
+</div>
+<div class="left-main">
+<div class="card-edit"> 
   <?php foreach($data['administradores'] as $adm): ?> 
-   <form action="./AdministradorController.php?action=update&id=<?= $adm->getId()?>" method="POST"> <!--  &id= -->
-	<label for="idn">Nome: </label> 
+    <div class="textfield"> 
+     <form action="./AdministradorController.php?action=update&id=<?= $adm->getId()?>" method="POST"> 
+	 <label for="idn">Nome: </label> 
      <input type="text" name="nome" id="idn" value="<?= $adm->getNome(); ?>">
       <p/>
       <label for="ids">Senha:</label>
      <input type="password" name="senha" id="ids" value="<?= $adm->getSenha(); ?>">
     <p/>
-   <input type="submit" value="Atualizar">
-  <input type="reset" value="Limpar">
+    <input type="submit" value="Atualizar">
+    </div>
+    </div>
+   
+  <!-- input type="reset" value="Limpar"-->
+  </div>
+  </div>
  </form>		
 <?php endforeach; ?>
+</div>
 </div>
 </body>
 </html>
