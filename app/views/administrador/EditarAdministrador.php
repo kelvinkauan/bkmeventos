2617a3e264c2c405bbca42e5570094cb5e47a230
Matheus Santos lopes <55161799+mattheusMSL@users.noreply.github.com>
2022-11-17 11:45:09 +0000

    atualização no editar adm

--- app/views/administrador/EditarAdministrador.php
+++ app/views/administrador/EditarAdministrador.php
@@ -4,16 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>editar dados administrador</title>
+    <title>editar administrador</title>
 </head>
 <body>
 <?php
 
 include_once __DIR__ . "/../helpers/mensagem.php";
 
+
 ?>
-<h2> Editar adms </h2>
- <p/>
+<div class="main-edit"> 
+    <div class="middle-main">  
+<h2> Editar administrador </h2>
+<p/>
+</div>
   <?php foreach($data['administradores'] as $adm): ?> 
    <form action="./AdministradorController.php?action=update&id=<?= $adm->getId()?>" method="POST"> <!--  &id= -->
 	<label for="idn">Nome: </label> 
@@ -26,6 +30,6 @@ include_once __DIR__ . "/../helpers/mensagem.php";
   <input type="reset" value="Limpar">
  </form>		
 <?php endforeach; ?>
-
+</div>
 </body>
 </html>
