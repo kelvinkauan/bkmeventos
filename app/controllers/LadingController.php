b5f2675cfa35e6236906619079d22f9d440aceab
mattheusMSL <matthewpeterparker@gmail.com>
2022-12-02 18:32:35 -0300

    Criação do controller do Lading Page e deixando o site responsivel

---
+++ app/controllers/LadingController.php
@@ -0,0 +1,61 @@
+<?php 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    require_once __DIR__ . "./../repository/OrganizadorRepository.php";
+
+$lading = new ControllerLanding();
+
+class ControllerLanding{
+
+
+    function __construct(){
+
+     if(isset($_POST["action"])){
+        $action = $_POST["action"];  
+    } elseif (isset($_GET["action"])){
+        $action = $_GET["action"];
+    }
+
+    if(isset($action)){
+        $this->callAction($action);
+    }else{
+        $this->loadView("error/erro.php");
+        $msg = "<h2>Nenhuma ação a ser processada...<h2>"; 
+        print_r($msg);
+    }
+
+    }
+
+    public function callAction(string $functionName = null){
+
+        if (method_exists($this, $functionName)) {
+            $this->$functionName();
+        } else if(method_exists($this, "preventDefault")) {
+            $met = "preventDefault";
+            $this->$met();
+        } else {
+            throw new BadFunctionCallException("Usecase not exists");
+        }
+
+    }
+
+    public function loadView(string $path, array $data = null, string $msg = null){
+
+        $caminho = __DIR__ . "/../views/" . $path;
+        if(file_exists($caminho)){
+             require $caminho;
+        } else {
+            $this->loadView("error/erro.php");
+            echo ("<h2> Erro ao carregar a view<h2>");   
+        }
+
+    }
+    
+    private function loadForm(){ //loadFormNew
+
+        $this->loadView("landingPage/index.html", null, "teste");
+
+    }
+} 
--- app/index.php
+++ app/index.php
@@ -3,6 +3,8 @@
 /* Redireciona o usúario para o controller principal dele no caso o CRUD (create,read,update,delete) //colocar no header?action=findAll */
 
 //header("location: views/login/login.php");
-header("location: controllers/OrganizadorController.php?action=findAll");
+//header("location: controllers/OrganizadorController.php?action=findAll");
+
+header("location: controllers/LadingController.php?action=loadForm");
 
 //header("location: controllers/AdministradorController.php?action=findAll");
